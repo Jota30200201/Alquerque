b839d5d7137d92964bb3195797360cce3cb78157
j.o.pereira <j.o.pereira1>
2023-06-18 17:14:35 +0100

    commit MoveResult, MoveType, PieceType

---
+++ src/alquerque/PieceType.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alquerque;
+
+/**
+ *
+ * @author joaop
+ */
+public enum PieceType {
+    Black(1), White(-1);
+    
+    final int moveDir;
+    PieceType(int moveDir){
+        this.moveDir = moveDir;
+    }
+}
