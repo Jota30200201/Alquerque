3a14a68c84b0b517a1bd6b9cd4279fb445507011
Jota30200201 <101662254+Jota30200201@users.noreply.github.com>
2023-06-18 17:03:28 +0100

    Classe Piece and Tile

---
+++ src/alquerque/Tile.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Alquerque;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+
+
+
+/**
+ *
+ * @author MSI Gaming
+ */
+public class Tile extends Rectangle {
+    private Piece piece;
+    private int tileX, tileY;
+    
+    public boolean hasPiece(){
+        return piece !=null;
+    }
+    public Piece getPiece(){
+        return piece;
+    }
+    public void setPiece(Piece piece){
+        this.piece=piece;
+    }
+
+    public int getTileX() {
+        return tileX;
+    }
+
+    public int getTileY() {
+        return tileY;
+    }
+    
+    
+    public Tile(boolean light, int x, int y){
+        setWidth(Alquerque.Tile_Size);
+        setHeight(Alquerque.Tile_Size);
+        tileX=x;
+        tileY=y;
+        relocate(x * Alquerque.Tile_Size, y * Alquerque.Tile_Size);
+        
+        setFill(light ? Color.valueOf("#89CFF0") : Color.valueOf("#FFA500"));
+        
+        //setOpacity(0.5);
+    }
+    
+    
+    
+}
