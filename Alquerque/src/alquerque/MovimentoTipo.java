d65b38f41ea83f33bd4bb0cf92cdeaab0db5c8ed
Jota30200201 <101662254+Jota30200201@users.noreply.github.com>
2023-06-20 10:36:19 +0100

    Add files via upload
    Ficheiro final LDP alquerque para ver

---
+++ Alquerque/src/alquerque/MovimentoTipo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alquerque;
+
+/**
+ *
+ * @author joaorosa
+ */
+public enum MovimentoTipo {
+    Nenhum, Normal, mata;
+}
